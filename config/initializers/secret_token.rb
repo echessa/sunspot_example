deccddffc00f0d84f094ca118347d1935992e864
Joyce Echessa <jokhessa@yahoo.com>
2014-04-19 14:05:17 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SunspotExample::Application.config.secret_key_base = '2dad5105ecb1ade2ddbd9b622794a15adec4be46990828ec948a83774e2d16c8a0202d592aeaf943f04e901f009f2f68a51f221639d7c08e58600dba7ab05c4b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SunspotExample::Application.config.session_store :cookie_store, key: '_sunspot_example_session'
